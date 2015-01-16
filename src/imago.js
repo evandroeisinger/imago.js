b21ae6b25cc6571a2e8fe235240c913115f1dbb9
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-16 20:27:11 -0200

    improve the imago.js plugin process

--- src/imago.js
+++ src/imago.js
@@ -1,34 +1,67 @@
 !function(plugins, document) {
   'use strict';
-  
+
   window.Imago = function(image) {
     var self    = this,
         image   = image,
-        figure  = document.createElement('figure'),
-        actions = document.createElement('div'),
-        parent  = image.parentElement;
+        figure  = self.getParent(image, 'figure', 'figure'),
+        actions = self.getElement(figure, 'figure__actions', 'div');
 
-    figure.className += 'figure';
-    image.className  += 'figure__image';
-    actions.className = 'figure__actions';
+    if (!figure) {
+      figure = document.createElement('figure');
+      figure.className += 'figure';
+      image.parentElement.insertBefore(figure, image);
+      figure.appendChild(image);
+    }
 
-    parent.insertBefore(figure, image);
-    figure.appendChild(actions);
-    figure.appendChild(image);
+    if (!actions) {
+      actions = document.createElement('div'),
+      actions.className = 'figure__actions';
+      figure.appendChild(actions);
+    }
+    
+    if (!image.className.indexOf('figure__image') <= 0)
+      image.className = 'figure__image';
 
-    self.plugins  = {};
+    if (image.style.cssText.length)
+      image.style.cssText = "";
+    
+    while (actions.firstChild) {
+      actions.removeChild(actions.firstChild);
+    }
+
     self.elements = {
       image: image,
       figure: figure,
-      actions: actions,
-      parent: parent
+      actions: actions
     };
 
     image.onload = function() {
-      for (var plugin in plugins)
-        self.plugins[plugin] = new plugins[plugin](self);
+      console.log(plugins)
+      for (var name in plugins)
+        actions.appendChild(new plugins[name](self));
     }
   }
 
-  Imago.prototype.plugins = {};
+  Imago.prototype = {
+    getParent: function(element, className, nodeName) {
+      if (!element.parentElement || 
+          !element.parentElement.nodeName.toLowerCase() == nodeName ||
+          !element.parentElement.className.indexOf(className) <= 0)
+        return false;
+
+      return element.parentElement;
+    },
+
+    getElement: function(wrapper, className, nodeName) {
+      if (wrapper.children) {
+        for (var i = 0; i < wrapper.children.length; i++) {
+          if ( wrapper.children[i].nodeName.toLowerCase() == nodeName &&  wrapper.children[i].className.indexOf(className) <= 0)
+            return  wrapper.children[i];  
+        }
+      }
+
+      return false;
+    }
+  }
 }(window.imagoPlugins, document);
