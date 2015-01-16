d261add9f173d6ad3867ffa5e39d8c9aca24e195
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-16 15:06:46 -0200

    removal of the unnecessary export editing method

--- src/imago.js
+++ src/imago.js
@@ -1,40 +1,32 @@
 !function(plugins, document) {
-	window.Imago = function(image) {
-		var self 		= this,
-				image 	= image,
-				figure 	= document.createElement('figure'),
-				actions = document.createElement('div'),
-				parent 	= image.parentElement;
+  window.Imago = function(image) {
+    var self    = this,
+        image   = image,
+        figure  = document.createElement('figure'),
+        actions = document.createElement('div'),
+        parent  = image.parentElement;
 
-		figure.className += 'figure';
-		image.className  += 'figure__image';
-		actions.className = 'figure__actions';
+    figure.className += 'figure';
+    image.className  += 'figure__image';
+    actions.className = 'figure__actions';
 
-		parent.insertBefore(figure, image);
-		figure.appendChild(actions);
-		figure.appendChild(image);
+    parent.insertBefore(figure, image);
+    figure.appendChild(actions);
+    figure.appendChild(image);
 
-		self.plugins = {};
-		self.elements = {
-			image: image,
-			figure: figure,
-			actions: actions,
-			parent: parent
-		};
+    self.plugins  = {};
+    self.elements = {
+      image: image,
+      figure: figure,
+      actions: actions,
+      parent: parent
+    };
 
-		image.onload = function() {
-			for (plugin in plugins)
-				self.plugins[plugin] = new plugins[plugin](self);
-		}
+    image.onload = function() {
+      for (plugin in plugins)
+        self.plugins[plugin] = new plugins[plugin](self);
+    }
+  }
 
-		return {
-			export: function() {
-				for (plugin in self.plugins)
-					if (self.plugins[plugin].export)
-						return self.plugins[plugin].export();
-			}
-		}
-	}
-
-	Imago.prototype.plugins = {};
+  Imago.prototype.plugins = {};
 }(window.imagoPlugins, document);
